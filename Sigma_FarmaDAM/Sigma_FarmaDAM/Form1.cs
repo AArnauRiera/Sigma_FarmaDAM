f512bc210b398db163de72bf869e44d351b54a32
Arnau Riera <arnau15riera@gmail.com>
2017-11-22 17:23:35 +0100

    Started logn layout

--- Sigma_FarmaDAM/Sigma_FarmaDAM/Form1.cs
+++ Sigma_FarmaDAM/Sigma_FarmaDAM/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Sigma_FarmaDAM
 {
-    public partial class Form1 : Form
+    public partial class LoginUser : Form
     {
-        public Form1()
+        public LoginUser()
         {
             InitializeComponent();
         }
