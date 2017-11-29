9f224e0a57c4e7b357d44c3c047c9471daf094dd
David <xivi30@gmail.com>
2017-11-29 15:53:24 +0100

    CustomControls TextBox

---
+++ Sigma_FarmaDAM/CustomControls/FormulariBase.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace CustomControls
+{
+    partial class FormulariBase
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtbxEntry = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(72, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(60, 13);
+            this.lblDescription.TabIndex = 0;
+            this.lblDescription.Text = "Description";
+            // 
+            // txtbxEntry
+            // 
+            this.txtbxEntry.Location = new System.Drawing.Point(138, 6);
+            this.txtbxEntry.Name = "txtbxEntry";
+            this.txtbxEntry.Size = new System.Drawing.Size(254, 20);
+            this.txtbxEntry.TabIndex = 1;
+            // 
+            // FormulariBase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtbxEntry);
+            this.Controls.Add(this.lblDescription);
+            this.Name = "FormulariBase";
+            this.Size = new System.Drawing.Size(469, 34);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtbxEntry;
+    }
+}
