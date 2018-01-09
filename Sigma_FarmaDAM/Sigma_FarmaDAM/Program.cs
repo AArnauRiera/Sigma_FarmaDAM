f6f72d43d9d13ab3ff74953b66a1ceef30dd4b75
David <xivi30@gmail.com>
2018-01-09 17:17:41 +0100

    - maintain users

--- Sigma_FarmaDAM/Sigma_FarmaDAM/Program.cs
+++ Sigma_FarmaDAM/Sigma_FarmaDAM/Program.cs
@@ -16,7 +16,7 @@ namespace Sigma_FarmaDAM
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginUser());
+//Application.Run(new LoginUser());
         }
     }
 }
