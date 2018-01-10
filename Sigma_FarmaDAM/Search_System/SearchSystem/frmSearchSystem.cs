7e86c872d8039ab839caeb53453faad8f4d2fc56
cesc <cescolmos1997@gmail.com>
2018-01-10 18:12:13 +0100

    SearchSystem  mejoras

--- Sigma_FarmaDAM/Search_System/SearchSystem/frmSearchSystem.cs
+++ Sigma_FarmaDAM/Search_System/SearchSystem/frmSearchSystem.cs
@@ -26,6 +26,7 @@ namespace SearchSystem
             get { return Value; }
             set {Value  = value; }
         }
+        
        
         
         
@@ -37,19 +38,23 @@ namespace SearchSystem
             DBUtils.DBUtils DBUTILS = new DBUtils.DBUtils();
             ///***Search in BBDD***///
             if (Value.Equals("Drugs"))
+
             {
-                query = "select* from Drugs where Register_Number ='"+ txtSearch.Text + "'";
+                query = "select* from Drugs where Register_Number ='"+ txtSearch1.Text + "and Sanitary_Register_Number ='" + txtSearch2.Text + " and id_active ='" + txtSearch3.Text + "'";
             }
             if(Value.Equals("Clients"))
             {
-                query = "select* from Clients where   Name =  '" + txtSearch.Text + "'";
+             
+              query = "select* from Clients where   DNI =  '" + txtSearch1.Text + " and Name  ='" + txtSearch2.Text + " and lastName1 ='" + txtSearch3.Text + "'";
             }
             if (Value.Equals("Active_Principles"))
             {
-                query = "select* from Active_Principles  where id ='" + txtSearch.Text + "'";
+             
+                query = "select* from Active_Principles  where id ='" + txtSearch1.Text + " and Sanitary_Register_Number ='" + txtSearch2.Text + " and Name ='" + txtSearch3.Text + "'";
 
             }
-
+            
+           
             dts = DBUTILS.PortarPerConsulta(query);
             ///***Rows Count***///
             if (dts.Tables[0].Rows.Count != 0)
@@ -63,6 +68,33 @@ namespace SearchSystem
             dgView_List.AutoResizeRows(DataGridViewAutoSizeRowsMode.AllCellsExceptHeaders);
         }
 
+        /// ***UPDATE LABEL TEXT***///
+
+        private void Label_Update()
+        {
+            if (Value.Equals("Drugs"))
+
+            {
+                lblSearch1.Text = "Register Number";
+                lblSearch2.Text = "Sanitary Register Number";
+                lblSearch3.Text = "Denomination";
+               
+            }
+            if (Value.Equals("Clients"))
+            {
+                lblSearch1.Text = "DNI";
+                lblSearch2.Text = "Name";
+                lblSearch3.Text = "Last Name";
+            }
+            if (Value.Equals("Active_Principles"))
+            {
+                lblSearch1.Text = "ID";
+                lblSearch2.Text = "Sanitary Register Number";
+                lblSearch3.Text = "Name";
+
+           }
+        }
+
         ///***MAIN***///
         public frmSearchSystem()
         {
@@ -73,6 +105,12 @@ namespace SearchSystem
         {
 
             portardadesSQL();         
+        }
+
+        private void lblSearch1_TextChanged(object sender, EventArgs e)
+        {
+
+            Label_Update();
         }
 
         private void dgView_List_CellClick(object sender, DataGridViewCellEventArgs e)
