c49f37033a89c8758b11914138c4066551ce1065
David <xivi30@gmail.com>
2018-01-16 16:28:36 +0100

    - Quaery Clientes

--- Sigma_FarmaDAM/Mantein/frmQueryBase.Designer.cs
+++ Sigma_FarmaDAM/Mantein/frmQueryBase.Designer.cs
@@ -34,24 +34,9 @@
             this.btnSelect = new System.Windows.Forms.Button();
             this.pnlButtons = new System.Windows.Forms.Panel();
             this.pnlTB = new System.Windows.Forms.Panel();
-            this.txtDNI = new Sigma_Controls.TxtSigma();
-            this.lblDNI = new System.Windows.Forms.Label();
-            this.lblid = new System.Windows.Forms.Label();
-            this.txtid = new Sigma_Controls.TxtSigma();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtSigma2 = new Sigma_Controls.TxtSigma();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtSigma3 = new Sigma_Controls.TxtSigma();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtSigma4 = new Sigma_Controls.TxtSigma();
-            this.label5 = new System.Windows.Forms.Label();
-            this.txtSigma5 = new Sigma_Controls.TxtSigma();
-            this.lblNTS_Type = new System.Windows.Forms.Label();
-            this.txtSigma6 = new Sigma_Controls.TxtSigma();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgwDB)).BeginInit();
             this.pnlButtons.SuspendLayout();
-            this.pnlTB.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -103,165 +88,11 @@
             // 
             // pnlTB
             // 
-            this.pnlTB.Controls.Add(this.lblNTS_Type);
-            this.pnlTB.Controls.Add(this.txtSigma6);
-            this.pnlTB.Controls.Add(this.label5);
-            this.pnlTB.Controls.Add(this.txtSigma5);
-            this.pnlTB.Controls.Add(this.label4);
-            this.pnlTB.Controls.Add(this.txtSigma4);
-            this.pnlTB.Controls.Add(this.label3);
-            this.pnlTB.Controls.Add(this.txtSigma3);
-            this.pnlTB.Controls.Add(this.label2);
-            this.pnlTB.Controls.Add(this.txtSigma2);
-            this.pnlTB.Controls.Add(this.lblid);
-            this.pnlTB.Controls.Add(this.txtid);
-            this.pnlTB.Controls.Add(this.lblDNI);
-            this.pnlTB.Controls.Add(this.txtDNI);
             this.pnlTB.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pnlTB.Location = new System.Drawing.Point(0, 0);
             this.pnlTB.Name = "pnlTB";
             this.pnlTB.Size = new System.Drawing.Size(595, 171);
             this.pnlTB.TabIndex = 4;
-            // 
-            // txtDNI
-            // 
-            this.txtDNI.DBReference = "DNI";
-            this.txtDNI.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtDNI.Location = new System.Drawing.Point(95, 12);
-            this.txtDNI.Name = "txtDNI";
-            this.txtDNI.Placeholder = null;
-            this.txtDNI.Size = new System.Drawing.Size(100, 20);
-            this.txtDNI.TabIndex = 0;
-            this.txtDNI.Table = "Clients";
-            // 
-            // lblDNI
-            // 
-            this.lblDNI.AutoSize = true;
-            this.lblDNI.Location = new System.Drawing.Point(13, 18);
-            this.lblDNI.Name = "lblDNI";
-            this.lblDNI.Size = new System.Drawing.Size(26, 13);
-            this.lblDNI.TabIndex = 1;
-            this.lblDNI.Text = "DNI";
-            // 
-            // lblid
-            // 
-            this.lblid.AutoSize = true;
-            this.lblid.Location = new System.Drawing.Point(13, 44);
-            this.lblid.Name = "lblid";
-            this.lblid.Size = new System.Drawing.Size(15, 13);
-            this.lblid.TabIndex = 3;
-            this.lblid.Text = "id";
-            // 
-            // txtid
-            // 
-            this.txtid.DBReference = "id";
-            this.txtid.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtid.Location = new System.Drawing.Point(95, 38);
-            this.txtid.Name = "txtid";
-            this.txtid.Placeholder = null;
-            this.txtid.Size = new System.Drawing.Size(100, 20);
-            this.txtid.TabIndex = 2;
-            this.txtid.Table = "Clients";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 70);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "LastName1";
-            // 
-            // txtSigma2
-            // 
-            this.txtSigma2.DBReference = "lastName1";
-            this.txtSigma2.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtSigma2.Location = new System.Drawing.Point(95, 64);
-            this.txtSigma2.Name = "txtSigma2";
-            this.txtSigma2.Placeholder = null;
-            this.txtSigma2.Size = new System.Drawing.Size(100, 20);
-            this.txtSigma2.TabIndex = 4;
-            this.txtSigma2.Table = "Clients";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 96);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(61, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "LastName2";
-            // 
-            // txtSigma3
-            // 
-            this.txtSigma3.DBReference = "lastName2";
-            this.txtSigma3.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtSigma3.Location = new System.Drawing.Point(95, 90);
-            this.txtSigma3.Name = "txtSigma3";
-            this.txtSigma3.Placeholder = null;
-            this.txtSigma3.Size = new System.Drawing.Size(100, 20);
-            this.txtSigma3.TabIndex = 6;
-            this.txtSigma3.Table = "Clients";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(238, 18);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(35, 13);
-            this.label4.TabIndex = 9;
-            this.label4.Text = "Name";
-            // 
-            // txtSigma4
-            // 
-            this.txtSigma4.DBReference = "Name";
-            this.txtSigma4.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtSigma4.Location = new System.Drawing.Point(320, 12);
-            this.txtSigma4.Name = "txtSigma4";
-            this.txtSigma4.Placeholder = null;
-            this.txtSigma4.Size = new System.Drawing.Size(100, 20);
-            this.txtSigma4.TabIndex = 8;
-            this.txtSigma4.Table = "Clients";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(238, 44);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "NTS";
-            // 
-            // txtSigma5
-            // 
-            this.txtSigma5.DBReference = "NTS";
-            this.txtSigma5.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtSigma5.Location = new System.Drawing.Point(320, 38);
-            this.txtSigma5.Name = "txtSigma5";
-            this.txtSigma5.Placeholder = null;
-            this.txtSigma5.Size = new System.Drawing.Size(100, 20);
-            this.txtSigma5.TabIndex = 10;
-            this.txtSigma5.Table = "Clients";
-            // 
-            // lblNTS_Type
-            // 
-            this.lblNTS_Type.AutoSize = true;
-            this.lblNTS_Type.Location = new System.Drawing.Point(238, 70);
-            this.lblNTS_Type.Name = "lblNTS_Type";
-            this.lblNTS_Type.Size = new System.Drawing.Size(59, 13);
-            this.lblNTS_Type.TabIndex = 13;
-            this.lblNTS_Type.Text = "NTS_Type";
-            // 
-            // txtSigma6
-            // 
-            this.txtSigma6.DBReference = "NTS_Type";
-            this.txtSigma6.FieldType = Sigma_Controls.TxtSigma.FieldTypes.NSS;
-            this.txtSigma6.Location = new System.Drawing.Point(320, 64);
-            this.txtSigma6.Name = "txtSigma6";
-            this.txtSigma6.Placeholder = null;
-            this.txtSigma6.Size = new System.Drawing.Size(100, 20);
-            this.txtSigma6.TabIndex = 12;
-            this.txtSigma6.Table = "Clients";
             // 
             // frmQueryBase
             // 
@@ -276,8 +107,6 @@
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.dgwDB)).EndInit();
             this.pnlButtons.ResumeLayout(false);
-            this.pnlTB.ResumeLayout(false);
-            this.pnlTB.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -289,19 +118,5 @@
         private System.Windows.Forms.Button btnSelect;
         private System.Windows.Forms.Panel pnlButtons;
         private System.Windows.Forms.Panel pnlTB;
-        private System.Windows.Forms.Label lblDNI;
-        private Sigma_Controls.TxtSigma txtDNI;
-        private System.Windows.Forms.Label lblNTS_Type;
-        private Sigma_Controls.TxtSigma txtSigma6;
-        private System.Windows.Forms.Label label5;
-        private Sigma_Controls.TxtSigma txtSigma5;
-        private System.Windows.Forms.Label label4;
-        private Sigma_Controls.TxtSigma txtSigma4;
-        private System.Windows.Forms.Label label3;
-        private Sigma_Controls.TxtSigma txtSigma3;
-        private System.Windows.Forms.Label label2;
-        private Sigma_Controls.TxtSigma txtSigma2;
-        private System.Windows.Forms.Label lblid;
-        private Sigma_Controls.TxtSigma txtid;
     }
 }
---
+++ Sigma_FarmaDAM/Mantein/frmQueryClients.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Mantein
+{
+    class frmQueryClients : Mantein.frmQueryBase
+    {
+      public frmQueryClients(DataGridView dgw, string table) : base(dgw, table)
+        {
+            InitializeComponent();
+            _dgw = dgw;
+            Table = table;
+        }
+
+
+
+    }
+}
