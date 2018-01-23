2ee50708a5e145521ee6bb02c58f75acc6bd7de6
cesc <cescolmos1997@gmail.com>
2018-01-23 17:46:44 +0100

    Search System

--- Sigma_FarmaDAM/Search_System/SearchSystem/frmSearchSystem.cs
+++ Sigma_FarmaDAM/Search_System/SearchSystem/frmSearchSystem.cs
@@ -15,79 +15,53 @@ namespace SearchSystem
 {
     public partial class frmSearchSystem : Form
     {
-
         public DataSet dts;
-
         String query, Value;
-        
+         DBUtils.DBUtils DBUTILS = new DBUtils.DBUtils();
+
         #region
         public string changebutton
         {
             get { return Value; }
             set { Value = value; }
         }
-
-        /***CHANGE LABEL TEXT 1 ***///
+        ////***CHANGE LABEL TEXT 1 ***///
         public string LabelText1
         {
-            get
-            {
-                return lblSearch1.Text;
-            }
-            set
-            {
-                lblSearch1.Text = value;
-            }
+           get{ return lblSearch1.Text; }
+           set{ lblSearch1.Text = value;}
         }
-        /***CHANGE LABEL TEXT 2 ***///
+        ///***CHANGE LABEL TEXT 2 ***///
         public string LabelText2
         {
-            get
-            {
-                return lblSearch2.Text;
-            }
-            set
-            {
-                lblSearch2.Text = value;
-            }
+            get{ return lblSearch2.Text; }
+            set{ lblSearch2.Text = value; }
         }
-        /***CHANGE LABEL TEXT 3 ***///
+        ///***CHANGE LABEL TEXT 3 ***///
         public string LabelText3
         {
-            get
-            {
-                return lblSearch3.Text;
-            }
-            set
-            {
-                lblSearch3.Text = value;
-            }
+            get{ return lblSearch3.Text;}
+            set{ lblSearch3.Text = value;}
         }
         #endregion
 
         ///***FUNCTIONS***///
-
         #region
         ///*** FUNCTION SEARCH DATA IN BBDD***///
+        
         private void portardadesSQL()
         {
-            DBUtils.DBUtils DBUTILS = new DBUtils.DBUtils();
-            ///***Search in BBDD***///
             if (Value.Equals("Drugs"))
-
             {
                 query = "select* from Drugs where Register_Number ='" + txtSearch1.Text + "' and Sanitary_Register_Num ='" + txtSearch2.Text + "' and id_active ='" + txtSearch3.Text + "'";
             }
             else if (Value.Equals("Clients"))
             {
-
                 query = "select* from Clients where   DNI =  '" + txtSearch1.Text + "' and Name  ='" + txtSearch2.Text + "' and lastName1 ='" + txtSearch3.Text + "'";
             }
             else
             {
-
                 query = "select* from Active_Principles  where id ='" + txtSearch1.Text + "' and Sanitary_Register_Number ='" + txtSearch2.Text + "' and Name ='" + txtSearch3.Text + "'";
-
             }
             dts = DBUTILS.PortarPerConsulta(query);
             ///***Rows Count***///
@@ -109,21 +83,16 @@ namespace SearchSystem
         #region
         
         public frmSearchSystem()
-        {
-
-            InitializeComponent();
-           
-
+        { 
+            InitializeComponent();       
         }
       
         private void btnSearch_Click(object sender, EventArgs e)
         {
-
-            portardadesSQL();         
+           portardadesSQL();         
         }
         private void dgView_List_CellClick(object sender, DataGridViewCellEventArgs e)
         {
-
             foreach (DataGridViewBand band in dgView_List.Columns)
             {
                 band.ReadOnly = true;
--- Sigma_FarmaDAM/Search_System/SearchSystem/frmTest.cs
+++ Sigma_FarmaDAM/Search_System/SearchSystem/frmTest.cs
@@ -14,19 +14,16 @@ namespace SearchSystem
     public partial class frmTest : Form
     {
         String form = "";
+        public frmSearchSystem f = new frmSearchSystem();
         public frmTest()
         {
             InitializeComponent();
         }
-
-
         ///***BTN DRUGS CLICK***///
         #region
         private void btnDrugs_Click(object sender, EventArgs e)
         {
-            form = "Drugs";
-
-            frmSearchSystem f = new frmSearchSystem();
+            form = "Drugs";         
             f.changebutton = form;
             f.LabelText1 = "Register Number";
             f.LabelText2 = "Sanitary Register Number";
@@ -36,31 +33,22 @@ namespace SearchSystem
         #endregion
         ///***BTN CLIENT CLICK***///
          #region
-
         private void btnClient_Click(object sender, EventArgs e)
         {
-          form = "Clients";
-
-            frmSearchSystem f = new frmSearchSystem();
-
-                f.changebutton = form;
+            form = "Clients";
+            f.changebutton = form;
             f.LabelText1 = "DNI";
             f.LabelText2 = "Name";
             f.LabelText3 = "Last Name";
-
             f.Show();
         }
          #endregion
-
         ///***BTN ACTIVE CLICK***///
         #region
         private void btnActive_Click(object sender, EventArgs e)
         {
-
-            form = "Active_Principles";
-            frmSearchSystem f = new frmSearchSystem();
-           
-                f.changebutton = form;
+            form = "Active_Principles";     
+            f.changebutton = form;
             f.LabelText1 = "ID";
             f.LabelText2 = "Sanitary Register Number";
             f.LabelText3 = "Name";
