fb5568e711178255e13936db59b7e6918f816de8
Marc Carbonell <karbu96@gmail.com>
2018-01-31 16:11:18 +0100

    Mantein forms

--- Sigma_FarmaDAM/Mantein/frmManteinBase.Designer.cs
+++ Sigma_FarmaDAM/Mantein/frmManteinBase.Designer.cs
@@ -42,34 +42,38 @@
             // dgwBase
             // 
             this.dgwBase.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgwBase.Location = new System.Drawing.Point(12, 201);
+            this.dgwBase.Location = new System.Drawing.Point(16, 247);
+            this.dgwBase.Margin = new System.Windows.Forms.Padding(4);
             this.dgwBase.Name = "dgwBase";
-            this.dgwBase.Size = new System.Drawing.Size(688, 217);
+            this.dgwBase.Size = new System.Drawing.Size(917, 267);
             this.dgwBase.TabIndex = 0;
             // 
             // pnlButtons
             // 
             this.pnlButtons.Controls.Add(this.btnUpdate);
             this.pnlButtons.Controls.Add(this.btnGet);
-            this.pnlButtons.Location = new System.Drawing.Point(12, 150);
+            this.pnlButtons.Location = new System.Drawing.Point(16, 185);
+            this.pnlButtons.Margin = new System.Windows.Forms.Padding(4);
             this.pnlButtons.Name = "pnlButtons";
-            this.pnlButtons.Size = new System.Drawing.Size(688, 45);
+            this.pnlButtons.Size = new System.Drawing.Size(917, 55);
             this.pnlButtons.TabIndex = 1;
             // 
             // btnUpdate
             // 
-            this.btnUpdate.Location = new System.Drawing.Point(413, 11);
+            this.btnUpdate.Location = new System.Drawing.Point(551, 14);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
             this.btnUpdate.Name = "btnUpdate";
-            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.Size = new System.Drawing.Size(100, 28);
             this.btnUpdate.TabIndex = 13;
             this.btnUpdate.Text = "Update";
             this.btnUpdate.UseVisualStyleBackColor = true;
             // 
             // btnGet
             // 
-            this.btnGet.Location = new System.Drawing.Point(201, 11);
+            this.btnGet.Location = new System.Drawing.Point(268, 14);
+            this.btnGet.Margin = new System.Windows.Forms.Padding(4);
             this.btnGet.Name = "btnGet";
-            this.btnGet.Size = new System.Drawing.Size(75, 23);
+            this.btnGet.Size = new System.Drawing.Size(100, 28);
             this.btnGet.TabIndex = 12;
             this.btnGet.Text = "Get";
             this.btnGet.UseVisualStyleBackColor = true;
@@ -78,16 +82,18 @@
             // pnlTextBox
             // 
             this.pnlTextBox.Controls.Add(this.btnQuery);
-            this.pnlTextBox.Location = new System.Drawing.Point(13, 13);
+            this.pnlTextBox.Location = new System.Drawing.Point(17, 16);
+            this.pnlTextBox.Margin = new System.Windows.Forms.Padding(4);
             this.pnlTextBox.Name = "pnlTextBox";
-            this.pnlTextBox.Size = new System.Drawing.Size(687, 131);
+            this.pnlTextBox.Size = new System.Drawing.Size(916, 161);
             this.pnlTextBox.TabIndex = 2;
             // 
             // btnQuery
             // 
-            this.btnQuery.Location = new System.Drawing.Point(599, 12);
+            this.btnQuery.Location = new System.Drawing.Point(799, 15);
+            this.btnQuery.Margin = new System.Windows.Forms.Padding(4);
             this.btnQuery.Name = "btnQuery";
-            this.btnQuery.Size = new System.Drawing.Size(75, 23);
+            this.btnQuery.Size = new System.Drawing.Size(100, 28);
             this.btnQuery.TabIndex = 0;
             this.btnQuery.Text = "Shearch";
             this.btnQuery.UseVisualStyleBackColor = true;
@@ -95,12 +101,13 @@
             // 
             // frmManteinBase
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 430);
+            this.ClientSize = new System.Drawing.Size(949, 529);
             this.Controls.Add(this.pnlTextBox);
             this.Controls.Add(this.pnlButtons);
             this.Controls.Add(this.dgwBase);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmManteinBase";
             this.Text = "frmManteinBase";
             ((System.ComponentModel.ISupportInitialize)(this.dgwBase)).EndInit();
@@ -114,9 +121,9 @@
 
         private System.Windows.Forms.DataGridView dgwBase;
         private System.Windows.Forms.Panel pnlButtons;
-        private System.Windows.Forms.Panel pnlTextBox;
         private System.Windows.Forms.Button btnUpdate;
         private System.Windows.Forms.Button btnGet;
         private System.Windows.Forms.Button btnQuery;
+        protected System.Windows.Forms.Panel pnlTextBox;
     }
 }
