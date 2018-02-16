194efc9a6788b02d7e0ca1f6530f468354476ea7
cesc <cescolmos1997@gmail.com>
2018-02-16 18:20:33 +0100

    Revert "SellSystem Update"
    This reverts commit 66030863e2a05b15f0c5d2dc3683d3cbb52f4b22.

--- Sigma_FarmaDAM/SellSystem/SellSystem/frmSellsytem.Designer.cs
+++ Sigma_FarmaDAM/SellSystem/SellSystem/frmSellsytem.Designer.cs
@@ -31,83 +31,56 @@
             this.lblClient = new System.Windows.Forms.Label();
             this.lblDrug = new System.Windows.Forms.Label();
             this.txtClient = new System.Windows.Forms.TextBox();
-            this.txtCod = new System.Windows.Forms.TextBox();
+            this.txtProduct = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.txtCantidad = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.txtProd = new System.Windows.Forms.TextBox();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtLastname2 = new System.Windows.Forms.TextBox();
-            this.label4 = new System.Windows.Forms.Label();
-            this.txtLastName = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.btnAdd = new System.Windows.Forms.Button();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.dgView_Sell = new System.Windows.Forms.DataGridView();
             this.btnBuy = new System.Windows.Forms.Button();
+            this.lswDrugs = new System.Windows.Forms.ListView();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.dgView_Sell)).BeginInit();
             this.SuspendLayout();
             // 
             // lblClient
             // 
             this.lblClient.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblClient.AutoSize = true;
-            this.lblClient.Location = new System.Drawing.Point(67, 54);
+            this.lblClient.Location = new System.Drawing.Point(248, 105);
             this.lblClient.Name = "lblClient";
-            this.lblClient.Size = new System.Drawing.Size(29, 13);
+            this.lblClient.Size = new System.Drawing.Size(36, 13);
             this.lblClient.TabIndex = 0;
-            this.lblClient.Text = "NTS";
+            this.lblClient.Text = "Client ";
             // 
             // lblDrug
             // 
             this.lblDrug.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.lblDrug.AutoSize = true;
-            this.lblDrug.Location = new System.Drawing.Point(70, 112);
+            this.lblDrug.Location = new System.Drawing.Point(248, 68);
             this.lblDrug.Name = "lblDrug";
-            this.lblDrug.Size = new System.Drawing.Size(40, 13);
+            this.lblDrug.Size = new System.Drawing.Size(44, 13);
             this.lblDrug.TabIndex = 1;
-            this.lblDrug.Text = "Codigo";
+            this.lblDrug.Text = "Product";
             // 
             // txtClient
             // 
             this.txtClient.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtClient.Location = new System.Drawing.Point(102, 51);
+            this.txtClient.Location = new System.Drawing.Point(325, 102);
             this.txtClient.Name = "txtClient";
-            this.txtClient.Size = new System.Drawing.Size(60, 20);
+            this.txtClient.Size = new System.Drawing.Size(64, 20);
             this.txtClient.TabIndex = 2;
-            this.txtClient.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtClient_KeyDown);
             // 
-            // txtCod
+            // txtProduct
             // 
-            this.txtCod.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtCod.Location = new System.Drawing.Point(122, 109);
-            this.txtCod.Name = "txtCod";
-            this.txtCod.Size = new System.Drawing.Size(98, 20);
-            this.txtCod.TabIndex = 3;
-            this.txtCod.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtCod_KeyDown);
+            this.txtProduct.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.txtProduct.Location = new System.Drawing.Point(325, 65);
+            this.txtProduct.Name = "txtProduct";
+            this.txtProduct.Size = new System.Drawing.Size(64, 20);
+            this.txtProduct.TabIndex = 3;
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.txtCantidad);
-            this.panel1.Controls.Add(this.label5);
-            this.panel1.Controls.Add(this.label7);
-            this.panel1.Controls.Add(this.txtProd);
-            this.panel1.Controls.Add(this.label6);
-            this.panel1.Controls.Add(this.txtLastname2);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.txtLastName);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.txtName);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtClient);
-            this.panel1.Controls.Add(this.txtCod);
+            this.panel1.Controls.Add(this.txtProduct);
             this.panel1.Controls.Add(this.btnAdd);
             this.panel1.Controls.Add(this.lblDrug);
             this.panel1.Controls.Add(this.lblClient);
@@ -117,162 +90,47 @@
             this.panel1.Size = new System.Drawing.Size(723, 177);
             this.panel1.TabIndex = 10;
             // 
-            // txtCantidad
-            // 
-            this.txtCantidad.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtCantidad.Location = new System.Drawing.Point(569, 109);
-            this.txtCantidad.Name = "txtCantidad";
-            this.txtCantidad.Size = new System.Drawing.Size(42, 20);
-            this.txtCantidad.TabIndex = 24;
-            // 
-            // label5
-            // 
-            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(517, 112);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
-            this.label5.TabIndex = 23;
-            this.label5.Text = "Cantidad";
-            // 
-            // label7
-            // 
-            this.label7.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(63, 85);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(44, 13);
-            this.label7.TabIndex = 22;
-            this.label7.Text = "Product";
-            // 
-            // txtProd
-            // 
-            this.txtProd.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtProd.Enabled = false;
-            this.txtProd.Location = new System.Drawing.Point(287, 109);
-            this.txtProd.Name = "txtProd";
-            this.txtProd.Size = new System.Drawing.Size(224, 20);
-            this.txtProd.TabIndex = 21;
-            // 
-            // label6
-            // 
-            this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(235, 112);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(50, 13);
-            this.label6.TabIndex = 20;
-            this.label6.Text = "Producto";
-            // 
-            // txtLastname2
-            // 
-            this.txtLastname2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtLastname2.Enabled = false;
-            this.txtLastname2.Location = new System.Drawing.Point(528, 51);
-            this.txtLastname2.Name = "txtLastname2";
-            this.txtLastname2.Size = new System.Drawing.Size(64, 20);
-            this.txtLastname2.TabIndex = 17;
-            // 
-            // label4
-            // 
-            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label4.AutoSize = true;
-            this.label4.Enabled = false;
-            this.label4.Location = new System.Drawing.Point(432, 54);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 13);
-            this.label4.TabIndex = 16;
-            this.label4.Text = "Segundo Apellido";
-            // 
-            // txtLastName
-            // 
-            this.txtLastName.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtLastName.Enabled = false;
-            this.txtLastName.Location = new System.Drawing.Point(352, 51);
-            this.txtLastName.Name = "txtLastName";
-            this.txtLastName.Size = new System.Drawing.Size(65, 20);
-            this.txtLastName.TabIndex = 15;
-            // 
-            // label3
-            // 
-            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label3.AutoSize = true;
-            this.label3.Enabled = false;
-            this.label3.Location = new System.Drawing.Point(290, 54);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(44, 13);
-            this.label3.TabIndex = 14;
-            this.label3.Text = "Apellido";
-            // 
-            // txtName
-            // 
-            this.txtName.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.txtName.Enabled = false;
-            this.txtName.Location = new System.Drawing.Point(209, 51);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(68, 20);
-            this.txtName.TabIndex = 13;
-            // 
-            // label2
-            // 
-            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label2.AutoSize = true;
-            this.label2.Enabled = false;
-            this.label2.Location = new System.Drawing.Point(168, 54);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 13);
-            this.label2.TabIndex = 12;
-            this.label2.Text = "Nombre";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(63, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
-            this.label1.TabIndex = 11;
-            this.label1.Text = "Cliente";
-            // 
             // btnAdd
             // 
             this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnAdd.Location = new System.Drawing.Point(304, 141);
+            this.btnAdd.Location = new System.Drawing.Point(290, 128);
             this.btnAdd.Name = "btnAdd";
-            this.btnAdd.Size = new System.Drawing.Size(98, 29);
+            this.btnAdd.Size = new System.Drawing.Size(133, 29);
             this.btnAdd.TabIndex = 10;
-            this.btnAdd.Text = "Add";
+            this.btnAdd.Text = "Add ";
             this.btnAdd.UseVisualStyleBackColor = true;
-            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // panel2
             // 
-            this.panel2.Controls.Add(this.dgView_Sell);
             this.panel2.Controls.Add(this.btnBuy);
+            this.panel2.Controls.Add(this.lswDrugs);
             this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 176);
+            this.panel2.Location = new System.Drawing.Point(0, 177);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(723, 274);
+            this.panel2.Size = new System.Drawing.Size(723, 273);
             this.panel2.TabIndex = 11;
-            // 
-            // dgView_Sell
-            // 
-            this.dgView_Sell.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgView_Sell.Location = new System.Drawing.Point(24, 7);
-            this.dgView_Sell.Name = "dgView_Sell";
-            this.dgView_Sell.Size = new System.Drawing.Size(662, 226);
-            this.dgView_Sell.TabIndex = 13;
             // 
             // btnBuy
             // 
             this.btnBuy.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.btnBuy.Location = new System.Drawing.Point(287, 239);
+            this.btnBuy.Location = new System.Drawing.Point(302, 200);
             this.btnBuy.Name = "btnBuy";
-            this.btnBuy.Size = new System.Drawing.Size(133, 23);
+            this.btnBuy.Size = new System.Drawing.Size(100, 23);
             this.btnBuy.TabIndex = 12;
             this.btnBuy.Text = "Buy";
             this.btnBuy.UseVisualStyleBackColor = true;
-            this.btnBuy.Click += new System.EventHandler(this.btnBuy_Click);
+            // 
+            // lswDrugs
+            // 
+            this.lswDrugs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lswDrugs.Location = new System.Drawing.Point(98, 28);
+            this.lswDrugs.Name = "lswDrugs";
+            this.lswDrugs.Size = new System.Drawing.Size(551, 166);
+            this.lswDrugs.TabIndex = 11;
+            this.lswDrugs.UseCompatibleStateImageBehavior = false;
+            this.lswDrugs.View = System.Windows.Forms.View.List;
             // 
             // frmSellsytem
             // 
@@ -283,11 +141,9 @@
             this.Controls.Add(this.panel1);
             this.Name = "frmSellsytem";
             this.Text = "SellSystem";
-            this.Load += new System.EventHandler(this.frmSellsytem_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.dgView_Sell)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -297,24 +153,12 @@
         private System.Windows.Forms.Label lblClient;
         private System.Windows.Forms.Label lblDrug;
         private System.Windows.Forms.TextBox txtClient;
-        private System.Windows.Forms.TextBox txtCod;
+        private System.Windows.Forms.TextBox txtProduct;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Panel panel2;
         private System.Windows.Forms.Button btnBuy;
-        private System.Windows.Forms.DataGridView dgView_Sell;
-        private System.Windows.Forms.TextBox txtLastname2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox txtLastName;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox txtName;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.TextBox txtProd;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox txtCantidad;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ListView lswDrugs;
     }
 }
 
--- Sigma_FarmaDAM/SellSystem/SellSystem/frmSellsytem.cs
+++ Sigma_FarmaDAM/SellSystem/SellSystem/frmSellsytem.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DBUtils;
-using MySql.Data.MySqlClient;
 
 namespace SellSystem
 {
@@ -17,181 +16,85 @@ namespace SellSystem
         DBUtils.DBUtilities DBUTILS = new DBUtils.DBUtilities();
         frmActive f = new frmActive();
         public DataSet dts;
-        DataTable dt = new DataTable();
-        public DataRow dr;
+        public DataTable dt;
 
         String Querry;
         Boolean validate = false;
         Boolean Client = false;
-        int contador = 1;
-
-        public string[] values = new string[4];
 
         ///***FUNCTIONS***///
         #region    
         private Boolean Client_exist()
         {
-            Querry = "select* from Clients  where NTS ='" + txtClient.Text + "'";
+            Querry = "select* from Clients  where DNI ='" + txtClient.Text + "'";
             dts = DBUTILS.PortarPerConsulta(Querry);
-            if (dts.Tables[0].Rows.Count > 0) { validate = true; }
-            else { validate = false; }
+            if (dts.Tables[0].Rows.Count > 0){ validate = true;}
+            else
+            {
+                MessageBox.Show("The Client doesn't exist");
+                validate = false;
+            }
             return validate;
-        }
-        private String Client_ID(String id)
-        {
-            Querry = "select NTS  from Clients  where NTS ='" + txtClient.Text + "'";
-            dts = DBUTILS.PortarPerConsulta(Querry);
-            String ID = dts.Tables[0].Rows[0]["NTS"].ToString();
-            return ID;
         }
         private Boolean drug_exist(String drug)
         {
-            Querry = "select* from Drugs  where Register_Number='" + drug + "'";
+            Querry = "select* from Drugs  where Register_Number='" + drug + "'" ;
             dts = DBUTILS.PortarPerConsulta(Querry);
-            if (dts.Tables[0].Rows.Count > 0) { validate = true; }
-            else { validate = false; }
+            if (dts.Tables[0].Rows.Count > 0) {validate = true;}
+            else{validate = false;}
             return validate;
         }
         private Boolean stock(String drug)
         {
             Querry = "select * from Stock where id='" + drug + "'and stock >0'";
             dts = DBUTILS.PortarPerConsulta(Querry);
-            if (dts.Tables[0].Rows.Count > 0) { validate = true; }
-            else { validate = false; }
+            if (dts.Tables[0].Rows.Count > 0) {validate = true;}
+            else {validate = false;}
             return validate;
         }
-        private DataSet client()
-        {
-            DataSet dts;
-            Querry = "select Name,lastName1,lastName2,DNI from Clients where NTS = '" + txtClient.Text + "'";
-            dts = DBUTILS.PortarPerConsulta(Querry);
-            return dts;
-        }
-        private string name_product()
-        {
-            DataSet dts;
-            Querry = "select CommercialName from Drugs where Register_Number = '" + txtCod.Text + "'";
-            dts = DBUTILS.PortarPerConsulta(Querry);
-            string Name;
-            try
-            {
-                Name = dts.Tables[0].Rows[0][0].ToString();
-            }
-            catch
-            {
-                Name = "";
-            }
-                
-
-            return Name;
-        }
-
-        #endregion
+       
+       #endregion
         ///***MAIN***///
         #region
         public frmSellsytem()
         {
             InitializeComponent();
         }
+        private void lswDrugs_Layout(object sender, LayoutEventArgs e)
+        {
+            ColumnHeader columnHeader1 = new ColumnHeader();
+            lswDrugs.HeaderStyle = ColumnHeaderStyle.None;
+            columnHeader1.Width = 500;
+            this.lswDrugs.Columns.Add(columnHeader1);
+        }
+
         private void btnAdd_Click(object sender, EventArgs e)
         {
-
-            bool exist = false;
-            String Drug = txtCod.Text.ToString();
-
-            for (int row = 0; row < dgView_Sell.Rows.Count - 1; row++)
-            {
-                if (dgView_Sell.Rows[row].Cells[0].Value.ToString().Equals(Drug))
-                {
-                    exist = true;
-                    dgView_Sell.Rows[row].Cells[4].Value = txtCantidad.Text;
-                }
+            Client = Client_exist();
+            if (Client)
+            {     
+                String Drug = txtProduct.Text.ToString();
+                ListViewItem item = new ListViewItem(Drug);
+                lswDrugs.Items.AddRange(new ListViewItem[] { item });
             }
-            if (dts.Tables[0].Rows.Count != 0)
-            {
-                dgView_Sell.DataSource = dt;
-            }
-            else { MessageBox.Show("There are no values "); }
-            if (!(exist)&&string.IsNullOrWhiteSpace(txtProd.Text)) ;
-            {
-                contador = 1;
-                dr = dt.NewRow();
-                dr[0] = txtCod.Text;
-                dr[1] = txtProd.Text;
-                dr[2] = "1";
-                dr[3] = Client_ID(Drug);
-                dr[4] = txtCantidad.Text;
-                dt.Rows.Add(dr);
-                txtCod.Text = "";
-                txtProd.Text = "";
-                txtCantidad.Text = "1";
-            }
-
         }
         private void btnBuy_Click(object sender, EventArgs e)
         {
-            for (int row = 0; row < dgView_Sell.Rows.Count - 1; row++)
+            Boolean drug = false;
+            int contadors = 0;
+            foreach (ListViewItem itemRow in this.lswDrugs.Items)
             {
-
-                if (!drug_exist(dgView_Sell.Rows[row].Cells[0].Value.ToString()))
+                for (int i = 0; i < itemRow.SubItems.Count; i++)
                 {
-                    MessageBox.Show("the drug " + dgView_Sell.Rows[row].Cells[0].Value.ToString() + " doesn't exist");
+                    String Value = itemRow.SubItems[i].ToString();
+                   drug = drug_exist(Value); 
+                   if (drug == false){contadors++;}
                 }
             }
-
-        }
-
-        private void frmSellsytem_Load(object sender, EventArgs e)
-        {
-            dt.Columns.Add("ID_Stock ");
-            dt.Columns.Add("Name");
-            dt.Columns.Add("ID_Seller");
-            dt.Columns.Add("ID_client");
-            dt.Columns.Add("quantity");
-            txtCantidad.Text = "1";
-        }
-
-        private void txtCod_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                string productName = name_product();
-                if (!String.IsNullOrEmpty(productName))
-                {
-                    txtProd.Text = productName;
- 
-                }
-                else
-                {
-                    txtCod.Text = "";
-                    txtProd.Text = "";
-
-                }
-            }
-        }
-
-        private void txtClient_KeyDown(object sender, KeyEventArgs e)
-        {
-
-            if (e.KeyCode == Keys.Enter)
-            {
-                Client = Client_exist();
-                if (Client)
-                {
-                    txtClient.ReadOnly = true;
-                    dts = client();
-                    txtName.Text = dts.Tables[0].Rows[0][0].ToString();
-                    txtLastName.Text = dts.Tables[0].Rows[0][1].ToString();
-                    txtLastname2.Text = dts.Tables[0].Rows[0][2].ToString();
-                }
-                else
-                {
-                    MessageBox.Show("There Client doesn't exist");
-                }
-            }   
+            if (contadors > 0){MessageBox.Show("There are products that doesn't exist");}
+            else{MessageBox.Show("Bought Correctly");}
         }
     }
 }
-
  
 #endregion
