bb1a0ecedea262a422a624f5d673d7913421bf0c
luigggi <32057267+luigggi@users.noreply.github.com>
2018-02-20 16:41:09 +0100

    Add files via upload

---
+++ ESCRIBIR_XML_FARMADAM/XMLTest/frmXMLReader.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmXMLReader
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(200, 47)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(83, 22)
+        Me.Button5.TabIndex = 5
+        Me.Button5.Text = "Crear XML"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'frmXMLReader
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(452, 95)
+        Me.Controls.Add(Me.Button5)
+        Me.Name = "frmXMLReader"
+        Me.Text = "Prova XMLReader"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+
+End Class
