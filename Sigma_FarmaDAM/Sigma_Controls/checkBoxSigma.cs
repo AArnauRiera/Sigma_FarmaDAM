52f353be86f1abc519ae0d5bd1a2f0f2278e3938
Arnau Riera <arnau15riera@gmail.com>
2018-02-28 16:23:08 +0100

    chcekboxsigma added

---
+++ Sigma_FarmaDAM/Sigma_Controls/checkBoxSigma.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sigma_Controls
+{
+    public class CheckBoxSigma : CheckBox
+    {
+        private string _DBReference;
+
+        public string DBReference
+        {
+            get { return _DBReference; }
+            set { _DBReference = value; }
+        }
+    }
+}
