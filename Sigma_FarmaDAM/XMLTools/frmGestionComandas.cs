e883cf1532afa45fb01ad5f8d89208303dfa8711
Unknown <karbu96@gmail.com>
2018-03-15 16:04:38 +0100

    Auto stash before merge of "master" and "origin/master"
    -FTP SMTTP getión de comandas

--- Sigma_FarmaDAM/XMLTools/frmGestionComandas.cs
+++ Sigma_FarmaDAM/XMLTools/frmGestionComandas.cs
@@ -26,20 +26,24 @@ namespace XMLTools
     private void GetCommanndByDate()
     {
 
-            DataSet ds;
-            XMLGenerator xmlGenerator = new XMLGenerator();
+        DataSet ds;
+        XMLGenerator xmlGenerator = new XMLGenerator();
 
-            dateTimePicker1.Format = DateTimePickerFormat.Custom;
-            dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+        dateTimePicker1.Format = DateTimePickerFormat.Custom;
+        dateTimePicker1.CustomFormat = "yyyy-MM-dd";
 
-            DBUtils.DBUtilities dbUtilities = new DBUtilities();
+        DBUtils.DBUtilities dbUtilities = new DBUtilities();
 
-            ds = dbUtilities.PortarPerConsulta(
-            "SELECT Drugs.CommercialName, Drugs.Sanitary_Register_Num, SUM(Order_Content.Quantity) AS Quantity, Drugs.NeedsRecipe FROM Order_Header, Order_Content, Drugs WHERE Order_Header.Id_Header = Order_Content.Id_Header AND Drugs.id = Order_Content.Id_Drug AND Date LIKE '%" + dateTimePicker1.AccessibilityObject.Value + "%' GROUP BY Order_Content.Id_Drug, Drugs.CommercialName, Drugs.Sanitary_Register_Num");
+        ds = dbUtilities.PortarPerConsulta(
+        "SELECT Drugs.CommercialName, Drugs.Sanitary_Register_Num, SUM(Order_Content.Quantity) AS Quantity, Drugs.NeedsRecipe FROM Order_Header, Order_Content, Drugs WHERE Order_Header.Id_Header = Order_Content.Id_Header AND Drugs.id = Order_Content.Id_Drug AND Date LIKE '%" + dateTimePicker1.AccessibilityObject.Value + "%' GROUP BY Order_Content.Id_Drug, Drugs.CommercialName, Drugs.Sanitary_Register_Num");
 
-            xmlGenerator.GenerateCommand(ds, dateTimePicker1.Text);
+        xmlGenerator.GenerateCommand(ds, dateTimePicker1.Text);
 
-        }
+        FTPHelper.UploadFile("Comanda_" + dateTimePicker1.Text + ".xml");
+
+        SMTPHelper.SendEmail();
+
+    }
 
     }
 }
