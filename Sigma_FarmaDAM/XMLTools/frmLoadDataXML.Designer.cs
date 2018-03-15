301d5f248610094963e726e7743792f9b49027b1
David <xivi30@gmail.com>
2018-03-15 18:32:09 +0100

    - xml upload

--- Sigma_FarmaDAM/XMLTools/frmLoadDataXML.Designer.cs
+++ Sigma_FarmaDAM/XMLTools/frmLoadDataXML.Designer.cs
@@ -30,16 +30,16 @@
         {
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.btnSelectXmlLab = new System.Windows.Forms.Button();
-            this.txtbxXml = new System.Windows.Forms.TextBox();
+            this.txtbxXmlLab = new System.Windows.Forms.TextBox();
             this.btnUploadLab = new System.Windows.Forms.Button();
-            this.lblCounter = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
+            this.lblLab = new System.Windows.Forms.Label();
+            this.lblPa = new System.Windows.Forms.Label();
             this.btnUploadPA = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txbPA = new System.Windows.Forms.TextBox();
             this.btnSelectXmlPA = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
+            this.lblDrug = new System.Windows.Forms.Label();
             this.btnUploadDrug = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txbDrug = new System.Windows.Forms.TextBox();
             this.btnSelectXmlDrug = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -52,124 +52,117 @@
             // 
             // btnSelectXmlLab
             // 
-            this.btnSelectXmlLab.Location = new System.Drawing.Point(355, 225);
-            this.btnSelectXmlLab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectXmlLab.Location = new System.Drawing.Point(266, 183);
             this.btnSelectXmlLab.Name = "btnSelectXmlLab";
-            this.btnSelectXmlLab.Size = new System.Drawing.Size(140, 28);
+            this.btnSelectXmlLab.Size = new System.Drawing.Size(105, 23);
             this.btnSelectXmlLab.TabIndex = 1;
             this.btnSelectXmlLab.Text = "Seleccionar Xml";
             this.btnSelectXmlLab.UseVisualStyleBackColor = true;
             this.btnSelectXmlLab.Click += new System.EventHandler(this.btnSelectXml_Click);
             // 
-            // txtbxXml
+            // txtbxXmlLab
             // 
-            this.txtbxXml.Location = new System.Drawing.Point(20, 227);
-            this.txtbxXml.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtbxXml.Name = "txtbxXml";
-            this.txtbxXml.Size = new System.Drawing.Size(323, 22);
-            this.txtbxXml.TabIndex = 2;
+            this.txtbxXmlLab.Location = new System.Drawing.Point(15, 184);
+            this.txtbxXmlLab.Name = "txtbxXmlLab";
+            this.txtbxXmlLab.Size = new System.Drawing.Size(243, 20);
+            this.txtbxXmlLab.TabIndex = 2;
             // 
             // btnUploadLab
             // 
-            this.btnUploadLab.Location = new System.Drawing.Point(161, 126);
-            this.btnUploadLab.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUploadLab.Location = new System.Drawing.Point(121, 102);
             this.btnUploadLab.Name = "btnUploadLab";
-            this.btnUploadLab.Size = new System.Drawing.Size(100, 28);
+            this.btnUploadLab.Size = new System.Drawing.Size(75, 23);
             this.btnUploadLab.TabIndex = 3;
             this.btnUploadLab.Text = "Subir Xml";
             this.btnUploadLab.UseVisualStyleBackColor = true;
             this.btnUploadLab.Click += new System.EventHandler(this.btnUpload_Click);
             // 
-            // lblCounter
+            // lblLab
             // 
-            this.lblCounter.AutoSize = true;
-            this.lblCounter.Location = new System.Drawing.Point(180, 293);
-            this.lblCounter.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblCounter.Name = "lblCounter";
-            this.lblCounter.Size = new System.Drawing.Size(0, 17);
-            this.lblCounter.TabIndex = 4;
+            this.lblLab.AutoSize = true;
+            this.lblLab.Location = new System.Drawing.Point(135, 238);
+            this.lblLab.Name = "lblLab";
+            this.lblLab.Size = new System.Drawing.Size(0, 13);
+            this.lblLab.TabIndex = 4;
             // 
-            // label1
+            // lblPa
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(680, 293);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
-            this.label1.TabIndex = 8;
+            this.lblPa.AutoSize = true;
+            this.lblPa.Location = new System.Drawing.Point(510, 238);
+            this.lblPa.Name = "lblPa";
+            this.lblPa.Size = new System.Drawing.Size(0, 13);
+            this.lblPa.TabIndex = 8;
             // 
             // btnUploadPA
             // 
-            this.btnUploadPA.Location = new System.Drawing.Point(661, 126);
-            this.btnUploadPA.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUploadPA.Location = new System.Drawing.Point(496, 102);
             this.btnUploadPA.Name = "btnUploadPA";
-            this.btnUploadPA.Size = new System.Drawing.Size(100, 28);
+            this.btnUploadPA.Size = new System.Drawing.Size(75, 23);
             this.btnUploadPA.TabIndex = 7;
             this.btnUploadPA.Text = "Subir Xml";
             this.btnUploadPA.UseVisualStyleBackColor = true;
+            this.btnUploadPA.Click += new System.EventHandler(this.btnUploadPA_Click);
             // 
-            // textBox1
+            // txbPA
             // 
-            this.textBox1.Location = new System.Drawing.Point(520, 227);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(323, 22);
-            this.textBox1.TabIndex = 6;
+            this.txbPA.Location = new System.Drawing.Point(390, 184);
+            this.txbPA.Name = "txbPA";
+            this.txbPA.Size = new System.Drawing.Size(243, 20);
+            this.txbPA.TabIndex = 6;
             // 
             // btnSelectXmlPA
             // 
-            this.btnSelectXmlPA.Location = new System.Drawing.Point(855, 225);
-            this.btnSelectXmlPA.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelectXmlPA.Location = new System.Drawing.Point(641, 183);
             this.btnSelectXmlPA.Name = "btnSelectXmlPA";
-            this.btnSelectXmlPA.Size = new System.Drawing.Size(140, 28);
+            this.btnSelectXmlPA.Size = new System.Drawing.Size(105, 23);
             this.btnSelectXmlPA.TabIndex = 5;
             this.btnSelectXmlPA.Text = "Seleccionar Xml";
             this.btnSelectXmlPA.UseVisualStyleBackColor = true;
+            this.btnSelectXmlPA.Click += new System.EventHandler(this.btnSelectXmlPA_Click);
             // 
-            // label2
+            // lblDrug
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1181, 293);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(0, 17);
-            this.label2.TabIndex = 12;
+            this.lblDrug.AutoSize = true;
+            this.lblDrug.Location = new System.Drawing.Point(886, 238);
+            this.lblDrug.Name = "lblDrug";
+            this.lblDrug.Size = new System.Drawing.Size(0, 13);
+            this.lblDrug.TabIndex = 12;
             // 
             // btnUploadDrug
             // 
-            this.btnUploadDrug.Location = new System.Drawing.Point(1162, 126);
-            this.btnUploadDrug.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUploadDrug.Location = new System.Drawing.Point(872, 102);
             this.btnUploadDrug.Name = "btnUploadDrug";
-            this.btnUploadDrug.Size = new System.Drawing.Size(100, 28);
+            this.btnUploadDrug.Size = new System.Drawing.Size(75, 23);
             this.btnUploadDrug.TabIndex = 11;
             this.btnUploadDrug.Text = "Subir Xml";
             this.btnUploadDrug.UseVisualStyleBackColor = true;
+            this.btnUploadDrug.Click += new System.EventHandler(this.btnUploadDrug_Click);
             // 
-            // textBox2
+            // txbDrug
             // 
-            this.textBox2.Location = new System.Drawing.Point(1021, 227);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(323, 22);
-            this.textBox2.TabIndex = 10;
+            this.txbDrug.Location = new System.Drawing.Point(766, 184);
+            this.txbDrug.Name = "txbDrug";
+            this.txbDrug.Size = new System.Drawing.Size(243, 20);
+            this.txbDrug.TabIndex = 10;
             // 
             // btnSelectXmlDrug
             // 
-            this.btnSelectXmlDrug.Location = new System.Drawing.Point(1352, 221);
-            this.btnSelectXmlDrug.Margin = new System.Windows.Forms.Padding(4);
+            this.btnSelectXmlDrug.Location = new System.Drawing.Point(1014, 180);
             this.btnSelectXmlDrug.Name = "btnSelectXmlDrug";
-            this.btnSelectXmlDrug.Size = new System.Drawing.Size(140, 28);
+            this.btnSelectXmlDrug.Size = new System.Drawing.Size(105, 23);
             this.btnSelectXmlDrug.TabIndex = 9;
             this.btnSelectXmlDrug.Text = "Seleccionar Xml";
             this.btnSelectXmlDrug.UseVisualStyleBackColor = true;
+            this.btnSelectXmlDrug.Click += new System.EventHandler(this.btnSelectXmlDrug_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(151, 32);
+            this.label3.Location = new System.Drawing.Point(113, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(115, 20);
+            this.label3.Size = new System.Drawing.Size(100, 17);
             this.label3.TabIndex = 13;
             this.label3.Text = "Laboratorios";
             // 
@@ -177,9 +170,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(639, 32);
+            this.label4.Location = new System.Drawing.Point(479, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(159, 20);
+            this.label4.Size = new System.Drawing.Size(135, 17);
             this.label4.TabIndex = 14;
             this.label4.Text = "Principios activos";
             // 
@@ -187,33 +181,33 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1162, 32);
+            this.label5.Location = new System.Drawing.Point(872, 26);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(84, 20);
+            this.label5.Size = new System.Drawing.Size(73, 17);
             this.label5.TabIndex = 15;
             this.label5.Text = "Producto";
             // 
             // frmLoadDataXML
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1612, 485);
+            this.ClientSize = new System.Drawing.Size(1203, 394);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblDrug);
             this.Controls.Add(this.btnUploadDrug);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txbDrug);
             this.Controls.Add(this.btnSelectXmlDrug);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblPa);
             this.Controls.Add(this.btnUploadPA);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txbPA);
             this.Controls.Add(this.btnSelectXmlPA);
-            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.lblLab);
             this.Controls.Add(this.btnUploadLab);
-            this.Controls.Add(this.txtbxXml);
+            this.Controls.Add(this.txtbxXmlLab);
             this.Controls.Add(this.btnSelectXmlLab);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmLoadDataXML";
             this.Text = "frmLoadDataXML";
             this.ResumeLayout(false);
@@ -224,16 +218,16 @@
         #endregion
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.Button btnSelectXmlLab;
-        protected System.Windows.Forms.TextBox txtbxXml;
+        protected System.Windows.Forms.TextBox txtbxXmlLab;
         protected System.Windows.Forms.Button btnUploadLab;
-        public System.Windows.Forms.Label lblCounter;
-        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label lblLab;
+        public System.Windows.Forms.Label lblPa;
         protected System.Windows.Forms.Button btnUploadPA;
-        protected System.Windows.Forms.TextBox textBox1;
+        protected System.Windows.Forms.TextBox txbPA;
         private System.Windows.Forms.Button btnSelectXmlPA;
-        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label lblDrug;
         protected System.Windows.Forms.Button btnUploadDrug;
-        protected System.Windows.Forms.TextBox textBox2;
+        protected System.Windows.Forms.TextBox txbDrug;
         private System.Windows.Forms.Button btnSelectXmlDrug;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
--- Sigma_FarmaDAM/XMLTools/frmLoadDataXML.cs
+++ Sigma_FarmaDAM/XMLTools/frmLoadDataXML.cs
@@ -15,7 +15,6 @@ namespace XMLTools
 {
     public partial class frmLoadDataXML : Form
     {
-        protected int nodeCount;
         public frmLoadDataXML()
         {
             InitializeComponent();
@@ -28,23 +27,23 @@ namespace XMLTools
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
                 System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
-                txtbxXml.Text = openFileDialog1.FileName;
+                txtbxXmlLab.Text = openFileDialog1.FileName;
                 sr.Close();
             }
         }
 
         public virtual void btnUpload_Click(object sender, EventArgs e)
         {
-            Thread t = new Thread(UploadData); // Kick off a new thread
+            Thread t = new Thread(UploadDataLab);
             t.Start();
         }
-        public virtual void UploadData()
+        public virtual void UploadDataDrugs()
         {
 
             try
             {
                 XmlDocument reader = new XmlDocument();
-                reader.Load(txtbxXml.Text);
+                reader.Load(txbDrug.Text);
 
                 DBUtilities db = new DBUtilities();
 
@@ -54,9 +53,9 @@ namespace XMLTools
 
                 int count = 0;
 
-                nodeCount = nodeList.Count;
+                int nodeCount = nodeList.Count;
 
-                Invoke((MethodInvoker)delegate { lblCounter.Text = count + " / " + nodeCount; });
+                Invoke((MethodInvoker)delegate { lblDrug.Text = count + " / " + nodeCount; });
 
                 foreach(XmlNode node in nodeList)
                 {
@@ -162,12 +161,12 @@ namespace XMLTools
 
                     count++;
 
-                    Invoke((MethodInvoker)delegate { lblCounter.Text = count + " / " + nodeCount; });
+                    Invoke((MethodInvoker)delegate { lblDrug.Text = count + " / " + nodeCount; });
                 }
 
                 db.Actualizar("select * from Drugs where 1 = 2", "Taula", ds);
 
-                Invoke((MethodInvoker)delegate { lblCounter.Text = "Finalizado!"; });
+                Invoke((MethodInvoker)delegate { lblDrug.Text = "Finalizado!"; });
 
             }
             catch(Exception ex)
@@ -177,6 +176,176 @@ namespace XMLTools
             
 
         }
+        public virtual void UploadDataLab()
+        {
+            try
+            {
+                XmlDocument reader = new XmlDocument();
+
+                reader.Load(txtbxXmlLab.Text);
+
+                DBUtilities db = new DBUtilities();
+
+                DataSet ds = db.PortarPerConsulta("select * from Laboratories where 1 = 2");
+
+                XmlNodeList nodeList = reader.DocumentElement.GetElementsByTagName("laboratorios");
+
+                int count = 0;
+
+                int nodeCount = nodeList.Count;
+
+                Invoke((MethodInvoker)delegate { lblLab.Text = count + " / " + nodeCount; });
+
+                foreach(XmlNode node in nodeList)
+                {
+                    DataRow dtr = ds.Tables["Taula"].NewRow();
+
+                    foreach(XmlNode child in node.ChildNodes)
+                    {
+                        string value;
+
+                        switch(child.Name)
+                        {
+                            case "codigolaboratorio":
+                                value = child.InnerText;
+                                dtr["id"] = value;
+                                break;
+                            case "laboratorio":
+                                value = child.InnerText;
+                                dtr["LabName"] = value;
+                                break;
+                            case "direccion":
+                                value = child.InnerText;
+                                dtr["Address"] = value;
+                                break;
+                            case "codigopostal":
+                                value = child.InnerText;
+                                dtr["cp"] = value;
+                                break;
+                            case "localidad":
+                                value = child.InnerText;
+                                dtr["Location"] = value;
+                                break;
+                            case "cif":
+                                value = child.InnerText;
+                                dtr["CIF"] = value;
+                                break;
+                        }
+                    }
+
+                    try
+                    {
+                        string query = "select * from Laboratories where id = " + "'" + dtr["id"] + "'";
+
+                        DataSet exist = db.PortarPerConsulta(query);
+
+                        if(exist.Tables["Taula"].Rows.Count == 0)
+                        {
+                            ds.Tables["Taula"].Rows.Add(dtr);
+                        }
+                    }
+                    catch(Exception e)
+                    {
+
+                        Console.WriteLine(e);
+                    }
+
+                    count++;
+
+                    Invoke((MethodInvoker)delegate { lblLab.Text = count + " / " + nodeCount; });
+                }
+
+                db.Actualizar("select * from Laboratories where 1 = 2", "Taula", ds);
+
+                Invoke((MethodInvoker)delegate { lblLab.Text = "Finalizado!"; });
+            }
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+        public virtual void UploadDataPA()
+        {
+            try
+            {
+                XmlDocument reader = new XmlDocument();
+
+                reader.Load(txbPA.Text);
+
+                DBUtilities db = new DBUtilities();
+
+                DataSet ds = db.PortarPerConsulta("select * from Active_Principles where 1 = 2");
+
+                XmlNodeList nodeList = reader.DocumentElement.GetElementsByTagName("principiosactivos");
+
+                int count = 0;
+
+                int nodeCount = nodeList.Count;
+
+                Invoke((MethodInvoker)delegate { lblPa.Text = count + " / " + nodeCount; });
+
+                foreach(XmlNode node in nodeList)
+                {
+                    DataRow dtr = ds.Tables["Taula"].NewRow();
+
+                    foreach(XmlNode child in node.ChildNodes)
+                    {
+                        string value;
+
+                        switch(child.Name)
+                        {
+                            case "nroprincipioactivo":
+                                value = child.InnerText;
+                                dtr["id"] = value;
+                                break;
+                            case "codigoprincipioactivo":
+                                value = child.InnerText;
+                                dtr["Sanitary_Register_Number"] = value;
+                                break;
+                            case "principioactivo":
+                                value = child.InnerText;
+                                dtr["Name"] = value;
+                                break;
+                        }
+                    }
+
+                    try
+                    {
+                        string query = "select * from Active_Principles where Sanitary_Register_Number = " + "'" + dtr["Sanitary_Register_Number"] + "'";
+
+                        DataSet exist = db.PortarPerConsulta(query);
+
+                        if(exist.Tables["Taula"].Rows.Count == 0)
+                        {
+                            ds.Tables["Taula"].Rows.Add(dtr);
+                        }
+
+                    }
+                    catch(Exception e)
+                    {
+
+                        Console.WriteLine(e);
+                    }
+
+                    count++;
+
+                    Invoke((MethodInvoker)delegate { lblPa.Text = count + " / " + nodeCount; });
+
+                }
+
+                db.Actualizar("select * from Active_Principles where 1 = 2", "Taula", ds);
+
+                Invoke((MethodInvoker)delegate { lblPa.Text = "Finalizado!"; });
+
+            }
+            catch(Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+
         private bool ConvertTo(string value)
         {
             if (value.Equals("1"))
@@ -186,6 +355,43 @@ namespace XMLTools
             else
             {
                 return false;
+            }
+        }
+
+        private void btnUploadPA_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(UploadDataPA);
+            t.Start();
+        }
+
+        private void btnUploadDrug_Click(object sender, EventArgs e)
+        {
+            Thread t = new Thread(UploadDataDrugs);
+            t.Start();
+        }
+
+        private void btnSelectXmlPA_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select a Xml file";
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                txbPA.Text = openFileDialog1.FileName;
+                sr.Close();
+            }
+
+        }
+
+        private void btnSelectXmlDrug_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Title = "Select a Xml file";
+
+            if(openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
+                txbDrug.Text = openFileDialog1.FileName;
+                sr.Close();
             }
         }
     }
