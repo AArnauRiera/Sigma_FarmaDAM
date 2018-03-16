a938f81c971f6edbeff2b3260d9c72eaac4bc5d1
David <xivi30@gmail.com>
2018-03-16 17:01:22 +0100

    Deleted maximize button in search form

--- Sigma_FarmaDAM/Mantein/frmQueryBase.Designer.cs
+++ Sigma_FarmaDAM/Mantein/frmQueryBase.Designer.cs
@@ -104,6 +104,7 @@
             this.Controls.Add(this.pnlButtons);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "frmQueryBase";
             this.Text = "Buscar";
             this.panel1.ResumeLayout(false);
