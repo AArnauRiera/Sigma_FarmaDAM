48124080a724d27b4c25cdf3ca930024cddc3d66
luigggi <lluisrubies@gmail.com>
2018-05-02 16:12:12 +0200

    Connection string done

--- Sigma_FarmaDAM/DBUtils/DBUtilities.cs
+++ Sigma_FarmaDAM/DBUtils/DBUtilities.cs
@@ -1,28 +1,27 @@
 ﻿using System;
 using System.Data;
 using MySql.Data.MySqlClient;
+using System.Configuration;
 
 namespace DBUtils
 {
     public class DBUtilities
     {
         #region Private Variables
+
         /// <summary>
-        /// String connection a la base de datos MySQL.
+        /// Cadena de conexion a la base de datos.
         /// </summary>
-        private string _stringMySQL = "SERVER=51.255.58.1;PORT=3306;DATABASE=g1s2am_FarmaDAM;Uid=g1s2am_SigmaCode;PASSWORD=12345aA";
+        private string _stringMySQL = ConfigurationManager.ConnectionStrings["sigma"].ConnectionString;
+
         /// <summary>
         /// Conexión a la base de datos.
         /// </summary>
         private MySqlConnection con;
+
         #endregion
 
         #region Public Variables
-        public string StringSQLOLEDB
-        {
-            get { return _stringMySQL; }
-            set { _stringMySQL = value; }
-        }
         #endregion
 
         #region OleDBMethods
